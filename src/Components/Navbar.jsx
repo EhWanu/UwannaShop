140476e4d851417a28da1bb00303b2eeb513645d
EhWanu <cameronjshadgett@gmail.com>
2021-09-13 15:58:07 -0400

    navbar skeleton layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,13 +6,37 @@ const Container = styled.div`
 `
 const Wrapper = styled.div`
     padding: 10px 20px;
+    display: flex; 
+    justify-content: space-between;
 `
+const Left = styled.div`
+    flex: 1;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const Center = styled.div`
+    flex: 1;
+
+`
+const Right = styled.div`
+    flex: 1;
+
+`
+
 
 const Navbar = () => {
     return (
         <Container>
             <Wrapper>
-                Navbar 
+                <Left>
+                    <Language>
+                        EN
+                    </Language>
+                </Left>
+                <Center>Center</Center>
+                <Right>Right</Right>
             </Wrapper> 
         </Container>
     )
