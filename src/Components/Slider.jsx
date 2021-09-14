b202fb2c6274db498fce634a95f52709d1e32e47
EhWanu <cameronjshadgett@gmail.com>
2021-09-14 13:23:06 -0400

    Slider skeleton outline

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -6,8 +6,8 @@ import styled from 'styled-components'
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
-    position: relative;
+     position: relative;
+     overflow: hidden;
  `
 
  const Arrow = styled.div`
@@ -24,7 +24,52 @@ import styled from 'styled-components'
     left: ${props => props.direction === "left" && "10px"};
     right: ${props => props.direction === "right" && "10px"};
     margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
  `
+
+ const Wrapper = styled.div`
+    height: 100%;
+    display: flex; 
+ `
+
+ const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+ `
+ const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+ `
+
+ const Image = styled.img`
+    height: 80%;
+ `
+
+ const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+ `
+
+ const Title = styled.h1`
+    font-size: 70px;
+ `
+ const Desc  = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+ `
+ const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer; 
+ `
+
+
  const Slider = () => {
      return (
          <div>
@@ -32,6 +77,38 @@ import styled from 'styled-components'
                 <Arrow direction="left">
                     <ArrowLeftOutlined />
                 </Arrow>
+                    <Wrapper>
+                        <Slide bg="f5fafd">
+                            <ImgContainer>
+                                <Image src='https://i.ibb.co/XsdmR2c/1.png'/>
+                            </ImgContainer>
+                            <InfoContainer>
+                                <Title>SUMMER SALE</Title>
+                                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS</Desc>
+                                <Button>SHOP NOW</Button>
+                            </InfoContainer>
+                        </Slide>
+                        <Slide>
+                            <ImgContainer>
+                                <Image src='https://i.ibb.co/XsdmR2c/1.png'/>
+                            </ImgContainer>
+                            <InfoContainer>
+                                <Title>WINTER SALE</Title>
+                                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS</Desc>
+                                <Button>SHOP NOW</Button>
+                            </InfoContainer>
+                        </Slide>
+                        <Slide>
+                            <ImgContainer>
+                                <Image src='https://i.ibb.co/XsdmR2c/1.png'/>
+                            </ImgContainer>
+                            <InfoContainer>
+                                <Title>POPULAR SALE</Title>
+                                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS</Desc>
+                                <Button>SHOP NOW</Button>
+                            </InfoContainer>
+                        </Slide> 
+                    </Wrapper>
                 <Arrow direction="right">
                     <ArrowRightOutlined />
                 </Arrow> 
