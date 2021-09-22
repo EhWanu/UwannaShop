55c808db6776b64d373cab3e7de4ae5bd49a00c8
EhWanu <cameronjshadgett@gmail.com>
2021-09-22 17:17:40 -0400

    cleaned up stylings

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -23,6 +23,34 @@ import Newsletter from "../Components/Newsletter"
  flex: 1;
  
 `
+const FilterContainer = styled.div`
+    display: flex;
+    margin: 30px 0px;
+    justify-content: space-between;
+    width: 50%;
+`
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`
+
+const FilterSize = styled.select `
+    margin-left: 10px;
+    padding: 10px;
+`
+const FilterSizeOption = styled.option``
 
  const Title = styled.h1`
     font-weight: 200;
@@ -52,7 +80,27 @@ const Product = () => {
                     <Title>Denim Jumpsuit</Title>
                     <Description>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error a laudantium minus. Repellat consectetur natus commodi quae aspernatur quam enim illo aperiam eaque mollitia. Quod sapiente exercitationem totam animi dolores!</Description>
                     <Price>$ 20</Price>
-                                        
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black" />
+                            <FilterColor color="darkblue" />
+                            <FilterColor color="gray" />
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        
+                    </AddContainer>                    
                 </InfoContainer>
             </Wrapper>
             <Newsletter />
