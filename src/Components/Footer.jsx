7619dc5cdccf37424f8471e9d5ed7272d4c2c565
EhWanu <cameronjshadgett@gmail.com>
2021-09-22 12:55:39 -0400

    contact skeleton

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons"
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
 import styled from "styled-components"
 
 const Container =  styled.div`
@@ -11,15 +11,52 @@ const Left =  styled.div`
     padding: 20px; 
 `
 const Logo = styled.h1``
-const Description = styled.p``
-const SocialContainer = styled.div``
-const SocialIcon = styled.div``
+
+const Description = styled.p`
+    margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right : 20px;
+    
+`
 
 const Center =  styled.div`
     flex: 1;
+    padding: 20px;
 `
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+    
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+
 const Right =  styled.div`
     flex: 1;
+    padding: 20px;
+
 `
     
 
@@ -35,22 +72,47 @@ const Footer = () => {
                     look remotely believable.
                 </Description>
                 <SocialContainer>
-                    <SocialIcon>
+                    <SocialIcon color ="3B5999">
                         <Facebook />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color ="E44054">
                         <Instagram />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color ="55ACEE">
                         <Twitter />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color ="E60023">
                         <Pinterest />
                     </SocialIcon>
                 </SocialContainer>
             </Left>
-            <Center></Center>
-            <Right></Right>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Men's Fashion</ListItem>
+                    <ListItem>Women's Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room /> 123 Fake Street
+                </ContactItem>
+                <ContactItem>
+                    <Phone /> +1 555 555 5555
+                </ContactItem>
+                <ContactItem>
+                   <MailOutline /> contact@uwanna.null
+                </ContactItem>
+                <Payment src='https://i.bb.co/Qfvn4z6/payment.png' />
+            </Right>
         </Container>
     )
 }
