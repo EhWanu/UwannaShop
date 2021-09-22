ecb096420dcb9f605ccfda2cfdffa11221875a3e
EhWanu <cameronjshadgett@gmail.com>
2021-09-21 22:37:40 -0400

    Product List layout

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -6,13 +6,13 @@ import CategoryItem from "./CategoryItem"
 const Container = styled.div`
     display: flex;
     padding: 20px;
-    justify-content: space-between;
+    justify-content: space-between ;
 `
 const Categories = () => {
     return (
         <Container>
             {categories.map(item => (
-                <CategoryItem item={item} />
+                <CategoryItem item={item} key={item.id} />
             ))} 
         </Container>
     )
