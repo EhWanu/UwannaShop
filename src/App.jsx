58ce0776002c189d814cda67659657802760c00a
EhWanu <cameronjshadgett@gmail.com>
2021-09-22 21:09:28 -0400

    boiler plate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Product from './pages/Product';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <Product  />
+    <Register  />
   );
 }
 
