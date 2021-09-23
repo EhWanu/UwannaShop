eb391294d428b006083d00fbde2bc92a52693b96
EhWanu <cameronjshadgett@gmail.com>
2021-09-22 21:29:59 -0400

    Login layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Product from './pages/Product';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Register  />
+    <Login  />
   );
 }
 
