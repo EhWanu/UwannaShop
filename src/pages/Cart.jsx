b75f1bdd51fe5c786ac7b7cecdb250a2a848354c
EhWanu <cameronjshadgett@gmail.com>
2021-09-23 10:11:42 -0400

    left cart layout

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,14 +15,69 @@ const Top = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding: 20px;
 `
 const TopButton = styled.button`
     padding: 10px;
     font-weight: 600;
     cursor: pointer;
+    border: ${props => props.type === 'filled' && 'none'};
+    background-color: ${props => props.type === 'filled' ? 'black' : 'transparent'};
+    color: ${props => props.type === 'filled' && 'white'};
 `
 
-const Bottom = styled.div``
+const TopTexts = styled.div``
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0 10px;
+`
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Info = styled.div`
+    flex: 3;
+`
+const Summary = styled.div`
+    flex: 1;
+`
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`
+const Image = styled.img`
+    width: 200px;
+`
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName = styled.span``
+const ProductId = styled.span``
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color}
+`
+const ProductSize = styled.span``
+const PriceDetail = styled.span`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+
 
 const Cart = () => {
     return (
@@ -33,9 +88,29 @@ const Cart = () => {
                 <Title>YOUR BAG</Title>
                 <Top>
                     <TopButton>CONTINUE SHOPPING</TopButton>
-                    <TopButton>CHECKOUT NOW</TopButton>
+                    <TopTexts>
+                        <TopText>Shopping Bag(2)</TopText>
+                        <TopText>Your Wishlist(0)</TopText>
+                    </TopTexts>
+                    <TopButton type='filled'>CHECKOUT NOW</TopButton>
                 </Top>
-                <Bottom></Bottom>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src='https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A'/>
+                                <Details>
+                                    <ProductName><b>Product:</b>JESSIE THUNDER SHOES</ProductName>
+                                    <ProductId><b>ID</b>98765674</ProductId>
+                                    <ProductColor color ='black'/>
+                                    <ProductSize><b>SIZE</b>37.5 </ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>price</PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>summary</Summary>
+                </Bottom>
             </Wrapper>
             <Footer />
         </Container>
